3922e753fb9fb23e6b3de1843b6694c92c7afa23
45uperman <orsuperman@gmail.com>
2025-05-20 23:09:03 -0500

    added some functionality to validateChirp to allow for a profanity filter + minor refactoring

--- main.go
+++ main.go
@@ -4,20 +4,81 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"slices"
+	"strings"
+	"sync"
 	"sync/atomic"
+	"unicode"
 )
 
 type Chirp struct {
 	Body string `json:"body"`
 }
 
+func (c *Chirp) clean(profaneWords []string, censorStr string) {
+	currentWord := ""
+	startIndex := -1
+	endIndex := 0
+	i := 0
+	for i <= len(c.Body) {
+		if i == len(c.Body) && startIndex >= 0 {
+			c.cleanWord(
+				profaneWords,
+				censorStr,
+				startIndex,
+				endIndex,
+			)
+
+			break
+		}
+
+		if unicode.IsSpace(rune(c.Body[i])) && startIndex >= 0 {
+			offset := c.cleanWord(
+				profaneWords,
+				censorStr,
+				startIndex,
+				endIndex,
+			)
+
+			i -= offset
+			currentWord = ""
+			startIndex = -1
+		} else {
+			currentWord += string(c.Body[i])
+			if startIndex == -1 {
+				startIndex = i
+			}
+			endIndex = i
+		}
+		i++
+	}
+}
+
+func (c *Chirp) cleanWord(profaneWords []string, censorStr string, startIndex, endIndex int) (offset int) {
+	currentWord := c.Body[startIndex : endIndex+1]
+
+	if slices.Contains(profaneWords, strings.ToLower(currentWord)) {
+		leftCut := c.Body[:startIndex]
+		rightCut := c.Body[endIndex+1:]
+
+		c.Body = strings.Join([]string{leftCut, rightCut}, censorStr)
+
+		offset = len(currentWord) - len(censorStr)
+	}
+
+	return
+}
+
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	charLimit      atomic.Int32
+	profaneWords   []string
+	censorStr      string
+	mu             sync.RWMutex
 }
 
 func (ac *apiConfig) validateChirpEndpoint(w http.ResponseWriter, req *http.Request) {
-	var requestChirp Chirp
+	var requestChirp *Chirp
 	decoder := json.NewDecoder(req.Body)
 	err := decoder.Decode(&requestChirp)
 	if err != nil {
@@ -27,11 +88,21 @@ func (ac *apiConfig) validateChirpEndpoint(w http.ResponseWriter, req *http.Requ
 	}
 
 	if len(requestChirp.Body) > int(ac.charLimit.Load()) {
-		w.WriteHeader(400)
-		w.Write([]byte(`{"error":"Chirp is too long"}`))
+		respondWithError(w, 400, "Chirp is too long")
 	} else {
-		w.WriteHeader(200)
-		w.Write([]byte(`{"valid":true}`))
+		ac.mu.RLock()
+		requestChirp.clean(ac.profaneWords, ac.censorStr)
+		ac.mu.RUnlock()
+
+		type returnVals struct {
+			CleanedBody string `json:"cleaned_body"`
+		}
+
+		respBody := returnVals{
+			CleanedBody: requestChirp.Body,
+		}
+
+		respondWithJson(w, 200, respBody)
 	}
 }
 
@@ -67,6 +138,8 @@ func (ac *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 func main() {
 	apiCfg := apiConfig{}
 	apiCfg.charLimit.Store(140)
+	apiCfg.profaneWords = []string{"kerfuffle", "sharbert", "fornax"}
+	apiCfg.censorStr = "****"
 
 	mux := http.NewServeMux()
 	server := http.Server{
@@ -99,4 +172,22 @@ func readinessEndpoint(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+	w.Write(fmt.Appendf(nil, `{"error":"%s"}`, msg))
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload any) error {
+	w.WriteHeader(code)
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	w.Write(data)
+
+	return nil
 }

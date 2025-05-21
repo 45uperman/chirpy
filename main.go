1183eebb9a1716c0a6c62e679482262c1c0ea15b
45uperman <orsuperman@gmail.com>
2025-05-21 09:49:17 -0500

    added a postgresql database with users and chirps + some endpoints to create them, absorbing some of the old functionality for chirps

--- main.go
+++ main.go
@@ -1,72 +1,39 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"slices"
 	"strings"
 	"sync"
 	"sync/atomic"
+	"time"
 	"unicode"
+
+	"github.com/45uperman/chirpy/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type ChirpyUser struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
 type Chirp struct {
-	Body string `json:"body"`
-}
-
-func (c *Chirp) clean(profaneWords []string, censorStr string) {
-	currentWord := ""
-	startIndex := -1
-	endIndex := 0
-	i := 0
-	for i <= len(c.Body) {
-		if i == len(c.Body) && startIndex >= 0 {
-			c.cleanWord(
-				profaneWords,
-				censorStr,
-				startIndex,
-				endIndex,
-			)
-
-			break
-		}
-
-		if unicode.IsSpace(rune(c.Body[i])) && startIndex >= 0 {
-			offset := c.cleanWord(
-				profaneWords,
-				censorStr,
-				startIndex,
-				endIndex,
-			)
-
-			i -= offset
-			currentWord = ""
-			startIndex = -1
-		} else {
-			currentWord += string(c.Body[i])
-			if startIndex == -1 {
-				startIndex = i
-			}
-			endIndex = i
-		}
-		i++
-	}
-}
-
-func (c *Chirp) cleanWord(profaneWords []string, censorStr string, startIndex, endIndex int) (offset int) {
-	currentWord := c.Body[startIndex : endIndex+1]
-
-	if slices.Contains(profaneWords, strings.ToLower(currentWord)) {
-		leftCut := c.Body[:startIndex]
-		rightCut := c.Body[endIndex+1:]
-
-		c.Body = strings.Join([]string{leftCut, rightCut}, censorStr)
-
-		offset = len(currentWord) - len(censorStr)
-	}
-
-	return
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
 }
 
 type apiConfig struct {
@@ -74,36 +41,92 @@ type apiConfig struct {
 	charLimit      atomic.Int32
 	profaneWords   []string
 	censorStr      string
+	dbQueries      *database.Queries
+	platform       string
 	mu             sync.RWMutex
 }
 
-func (ac *apiConfig) validateChirpEndpoint(w http.ResponseWriter, req *http.Request) {
-	var requestChirp *Chirp
+func (ac *apiConfig) createUserEndpoint(w http.ResponseWriter, req *http.Request) {
+	type reqVals struct {
+		Email string `json:"email"`
+	}
+
+	var rv reqVals
 	decoder := json.NewDecoder(req.Body)
-	err := decoder.Decode(&requestChirp)
+	err := decoder.Decode(&rv)
+	if err != nil {
+		fmt.Printf("Error decoding create user request: %s\n", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	if rv.Email == "" {
+		respondWithError(w, 400, "user needs an email")
+		return
+	}
+
+	dbUser, err := ac.dbQueries.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Email:     rv.Email,
+		},
+	)
+	if err != nil {
+		if err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"` {
+			respondWithError(w, 400, "email already taken")
+		}
+		fmt.Printf("Error decoding making CreateUser query: %s\n", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	respondWithJson(w, 201, ChirpyUser(dbUser))
+}
+
+func (ac *apiConfig) createChirpEndpoint(w http.ResponseWriter, req *http.Request) {
+	type reqVals struct {
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+
+	var rv reqVals
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&rv)
 	if err != nil {
 		fmt.Printf("Error decoding chirp: %s\n", err)
 		w.WriteHeader(500)
 		return
 	}
 
-	if len(requestChirp.Body) > int(ac.charLimit.Load()) {
-		respondWithError(w, 400, "Chirp is too long")
-	} else {
-		ac.mu.RLock()
-		requestChirp.clean(ac.profaneWords, ac.censorStr)
-		ac.mu.RUnlock()
-
-		type returnVals struct {
-			CleanedBody string `json:"cleaned_body"`
-		}
-
-		respBody := returnVals{
-			CleanedBody: requestChirp.Body,
-		}
-
-		respondWithJson(w, 200, respBody)
+	if len(rv.Body) > int(ac.charLimit.Load()) {
+		respondWithError(w, 400, "chirp is too long")
+		return
 	}
+
+	newChirp, err := ac.dbQueries.CreateChirp(
+		context.Background(),
+		database.CreateChirpParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Body: cleanChirpBody(
+				ac.profaneWords,
+				ac.censorStr,
+				rv.Body,
+			),
+			UserID: rv.UserID,
+		},
+	)
+	if err != nil {
+		fmt.Printf("Error inserting chirp into database: %s\n", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	respondWithJson(w, 201, Chirp(newChirp))
 }
 
 func (ac *apiConfig) metricsEndpoint(w http.ResponseWriter, req *http.Request) {
@@ -123,7 +146,22 @@ func (ac *apiConfig) metricsEndpoint(w http.ResponseWriter, req *http.Request) {
 }
 
 func (ac *apiConfig) resetEndpoint(w http.ResponseWriter, req *http.Request) {
+	if ac.platform != "dev" {
+		w.WriteHeader(403)
+		w.Write([]byte("Forbidden"))
+		return
+	}
+
 	ac.fileserverHits.Store(0)
+	err := ac.dbQueries.DeleteAllUsers(context.Background())
+	if err != nil {
+		fmt.Printf("Error resetting: %s\n", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
 
 func (ac *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -136,10 +174,19 @@ func (ac *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+
 	apiCfg := apiConfig{}
 	apiCfg.charLimit.Store(140)
 	apiCfg.profaneWords = []string{"kerfuffle", "sharbert", "fornax"}
 	apiCfg.censorStr = "****"
+	apiCfg.dbQueries = database.New(db)
+	apiCfg.platform = os.Getenv("PLATFORM")
 
 	mux := http.NewServeMux()
 	server := http.Server{
@@ -149,11 +196,13 @@ func main() {
 
 	mux.HandleFunc("GET /api/healthz", readinessEndpoint)
 
-	mux.HandleFunc("POST /api/validate_chirp", apiCfg.validateChirpEndpoint)
+	mux.HandleFunc("POST /api/chirps", apiCfg.createChirpEndpoint)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsEndpoint)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetEndpoint)
+
+	mux.HandleFunc("POST /api/users", apiCfg.createUserEndpoint)
 
 	mux.Handle(
 		"/app/",
@@ -190,4 +239,63 @@ func respondWithJson(w http.ResponseWriter, code int, payload any) error {
 	w.Write(data)
 
 	return nil
+}
+
+func cleanChirpBody(profaneWords []string, censorStr, body string) string {
+	currentWord := ""
+	startIndex := -1
+	endIndex := 0
+	i := 0
+	for i <= len(body) {
+		if i == len(body) && startIndex >= 0 {
+			cleanWord(
+				profaneWords,
+				censorStr,
+				&body,
+				startIndex,
+				endIndex,
+			)
+
+			break
+		}
+
+		if unicode.IsSpace(rune(body[i])) && startIndex >= 0 {
+			offset := cleanWord(
+				profaneWords,
+				censorStr,
+				&body,
+				startIndex,
+				endIndex,
+			)
+
+			i -= offset
+			currentWord = ""
+			startIndex = -1
+		} else {
+			currentWord += string(body[i])
+			if startIndex == -1 {
+				startIndex = i
+			}
+			endIndex = i
+		}
+		i++
+	}
+
+	return body
+}
+
+func cleanWord(profaneWords []string, censorStr string, body *string, startIndex, endIndex int) (offset int) {
+	derefBody := *body
+	currentWord := derefBody[startIndex : endIndex+1]
+
+	if slices.Contains(profaneWords, strings.ToLower(currentWord)) {
+		leftCut := derefBody[:startIndex]
+		rightCut := derefBody[endIndex+1:]
+
+		*body = strings.Join([]string{leftCut, rightCut}, censorStr)
+
+		offset = len(currentWord) - len(censorStr)
+	}
+
+	return
 }
